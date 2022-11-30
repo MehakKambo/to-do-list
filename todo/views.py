1970c1084312dd99d7675bceddf0b0b3dfdbcc82
MehakKambo <mehakpreetp12@gmail.com>
2022-11-29 22:54:09 -0800

    Display task data

--- todo/views.py
+++ todo/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Task
 
 # Create your views here.
+# Uses the task_list.html
 class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'
     
-    
-    
+# Uses the task_detail.html
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
