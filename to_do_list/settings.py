47da0741912419b1225619c4ec8ed0edacf925ad
MehakKambo <mehakpreetp12@gmail.com>
2022-12-01 12:43:53 -0800

    Add final touches

--- to_do_list/settings.py
+++ to_do_list/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%l%0umwx-6h8)5c=5g6moeggfti^fgnt(u_8!x_+8#^m^*c=wv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://todoolist.azurewebsites.net/', '127.0.0.1']
 
 
 # Application definition
--- todo/views.py
+++ todo/views.py
@@ -1,106 +1,43 @@
 from django.shortcuts import render, redirect
-from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
-from django.urls import reverse_lazy
-
-from django.contrib.auth.views import LoginView
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
-
-# Imports for Reordering Feature
-from django.views import View
-from django.shortcuts import redirect
-from django.db import transaction
-
-from .models import Task
-from .forms import PositionForm
+from django.http import HttpResponse
+from .models import *
+from .forms import *
 
 
-class CustomLoginView(LoginView):
-    template_name = 'todo/login.html'
-    fields = '__all__'
-    redirect_authenticated_user = True
+# Create your views here.
 
-    def get_success_url(self):
-        return reverse_lazy('tasks')
-
-
-class RegisterPage(FormView):
-    template_name = 'todo/register.html'
-    form_class = UserCreationForm
-    redirect_authenticated_user = True
-    success_url = reverse_lazy('tasks')
-
-    def form_valid(self, form):
-        user = form.save()
-        if user is not None:
-            login(self.request, user)
-        return super(RegisterPage, self).form_valid(form)
-
-    def get(self, *args, **kwargs):
-        if self.request.user.is_authenticated:
-            return redirect('tasks')
-        return super(RegisterPage, self).get(*args, **kwargs)
-
-
-class TaskList(LoginRequiredMixin, ListView):
-    model = Task
-    context_object_name = 'tasks'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['tasks'] = context['tasks'].filter(user=self.request.user)
-        context['count'] = context['tasks'].filter(complete=False).count()
-
-        search_input = self.request.GET.get('search-area') or ''
-        if search_input:
-            context['tasks'] = context['tasks'].filter(
-                title__contains=search_input)
-
-        context['search_input'] = search_input
-
-        return context
-
-
-class TaskDetail(LoginRequiredMixin, DetailView):
-    model = Task
-    context_object_name = 'task'
-
-
-class TaskCreate(LoginRequiredMixin, CreateView):
-    model = Task
-    fields = ['title', 'description', 'complete']
-    success_url = reverse_lazy('tasks')
-
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        return super(TaskCreate, self).form_valid(form)
-
-
-class TaskUpdate(LoginRequiredMixin, UpdateView):
-    model = Task
-    fields = ['title', 'description', 'complete']
-    success_url = reverse_lazy('tasks')
-
-
-class DeleteView(LoginRequiredMixin, DeleteView):
-    model = Task
-    context_object_name = 'task'
-    success_url = reverse_lazy('tasks')
-    def get_queryset(self):
-        owner = self.request.user
-        return self.model.objects.filter(user=owner)
-
-class TaskReorder(View):
-    def post(self, request):
-        form = PositionForm(request.POST)
-
+def index(request):
+    tasks = Task.objects.all()
+    form = TaskForm()
+    if request.method == "POST":
+        form = TaskForm(request.POST)
         if form.is_valid():
-            positionList = form.cleaned_data["position"].split(',')
+            form.save()
+        return redirect('/')
+    context = {'tasks': tasks, 'form': form}
+    return render(request, 'todo/index.html', context)
 
-            with transaction.atomic():
-                self.request.user.set_task_order(positionList)
 
-        return redirect(reverse_lazy('tasks'))
+def update_task(request, key):
+    task = Task.objects.get(id=key)
+    form = TaskForm(instance=task)
+
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form': form}
+    return render(request, "todo/update_task.html", context)
+
+
+def delete(request, key):
+    item = Task.objects.get(id=key)
+
+    if request.method == "POST":
+        item.delete()
+        return redirect('/')
+
+    context = {'item':item}
+    return render(request, "todo/delete.html", context)
